104d3ef2817423b5e89b71f9924651a4a26e2fe6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-04 21:39:21 +0100

    Update nl_NL, thanks to Kees Hessels

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CDN_USE_EXTRA_DESC'] = 'Selecteer om een extra Content Delivery Network setup mogelijk te maken. Op deze manier kunt u verschillende instellingen gebruiken voor verschillende bestandstypen, zoals aparte instellingen voor afbeeldingen en video-bestanden.';
+$lang['CDN_DOMAINS_DESC'] = 'Het domein van uw CDN-server.<br/>U kunt ook een door komma\'s gescheiden lijst van verschillende CDN domeinen aangeven. Een van deze zal worden toegewezen aan elk willekeurig bestand in de set.<br/><br/>U kunt de plaatshouders {subdomein}, {domain} en {extensie} gebruiken om te verwijzen naar de verschillende delen van de domeinnaam van uw website.';
+$lang['CDN_DOMAIN'] = 'CDN Domein';
+$lang['CDN_DOMAIN_DESC'] = 'Het domein (host) van uw CDN Server';
+$lang['CDN_ENABLED'] = 'Ingeschakeld CDN';
+$lang['CDN_ENABLED_DESC'] = 'Zorgt voor eenvoudige integratie met een Content Delivery Network (CDN).';
+$lang['CDN_EXTRA_FILETYPES'] = 'Extra bestandstypen';
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'Een door komma\'s gescheiden lijst met extra bestandstypen die moeten worden bediend vanaf uw CDN server, bv: png, jpg';
+$lang['CDN_KEEP_HTTPS_DESC'] = 'Selecteer om https in de cdn url\'s te behouden. Anders zullen de cdn-url\'s http gebruiken.';
+$lang['CDN_SITE_ROOT'] = 'Website Root';
+$lang['CDN_SITE_ROOT_DESC'] = 'De hoofdmap (root) van uw website die u verbonden hebt met uw CDN server. <br/> U hoeft dit waarschijnlijk niet te veranderen, maar als het nodig mocht zijn, kunt u een submap aangeven, zoals /afbeeldingen/.';
+$lang['CDN_THEME'] = 'Toepassen op thema';
+$lang['CDN_THEME_DESC'] = 'Exporteer de thema-bestanden, bv: js, css.';
+$lang['CDN_USE'] = 'CDN Opties';
+$lang['CDN_USE_EXTRA'] = 'Meerdere CDN roots';
+$lang['CDN_USE_EXTRA_2'] = 'Gebruik 2e CDN set';
+$lang['CDN_USE_EXTRA_3'] = 'Gebruik 3e CDN set';
+$lang['CDN_USE_EXTRA_4'] = 'Gebruik 4e CDN set';
+$lang['CDN_USE_EXTRA_5'] = 'Gebruik 5e CDN set';
+$lang['PLG_CDNPLUS'] = 'Deze plugin maakt een eenvoudige integratie met een Content Delivery Network (CDN) voor uw galerij mogelijk.';
+$lang['CDN_FILE_TYPES'] = 'Bestandstypen';
+$lang['CDN_FILE_TYPES_DESC'] = 'Selecteer de bestandstypen die moeten worden bediend vanaf uw CDN server.';
+$lang['CDN_IGNORE_FILES'] = 'Negeer bestanden';
+$lang['CDN_IGNORE_FILES_DESC'] = 'Een door komma\'s gescheiden lijst van te negeren pad/bestandsnamen (of een deel van pad/bestandsnamen)(url\'s zullen niet worden gewijzigd in CDN-server).';
+$lang['CDN_KEEP_HTTPS'] = 'Houd HTTPS in url\'s';
