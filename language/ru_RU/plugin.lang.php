88cf53e3de006352f04dd37726fa92f7eea9b175
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-04 21:39:17 +0100

    Update ru_RU, thanks to Konve

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CDN_DOMAINS_DESC'] = 'Домен Вашего сервера CDN. <br/> Вы можете  использовать список доменов CDN, разделенных запятыми. Один из них будет назначен для каждого файла наугад при запуске. <br/><br/>
+Вы можете использовать {поддомен}, {домен} и {расширение} для обозначения разных частей доменного имя Вашего веб-сайта.';
+$lang['CDN_USE_EXTRA_DESC'] = 'Выберите, чтобы включить расширенные настройки Content Delivery Network. Вы можете использовать различные настройки для различных типов файлов, например, разные параметры для изображений и для видео файлов.';
+$lang['CDN_IGNORE_FILES_DESC'] = 'Разделяйте запятыми список путей или имен файлов, которые должны игнорироваться сервером CDN.';
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'Разделяйте запятыми список типов файлов, которые должны обрабатываться Вашим сервером CDN, например: png, jpg.';
+$lang['CDN_KEEP_HTTPS_DESC'] = 'Отметьте, чтобы сохранять https в url-адресах. В противном случае, CDN будет использовать только http.';
+$lang['CDN_SITE_ROOT_DESC'] = 'Корень веб-сайта, который Вы подключили к Вашему серверу CDN.<br/>
+Вам, вероятно, не придется здесь что-то менять, но если Вам нужно - Вы можете указать подпапку, например /images/.';
+$lang['PLG_CDNPLUS'] = 'Этот плагин позволяет легко интегрировать Content Delivery Network (CDN) для Вашей галереи.';
+$lang['CDN_USE_EXTRA_3'] = 'Использовать 3-й набор CDN';
+$lang['CDN_USE_EXTRA_4'] = 'Использовать 4-й набор CDN';
+$lang['CDN_USE_EXTRA_5'] = 'Использовать 5-й набор CDN';
+$lang['CDN_USE_EXTRA_2'] = 'Использовать 2-й набор CDN';
+$lang['CDN_USE_EXTRA'] = 'Несколько корней CDN';
+$lang['CDN_USE'] = 'Параметры CDN';
+$lang['CDN_THEME_DESC'] = 'Экспортировать файлы темы, например: js, css.';
+$lang['CDN_THEME'] = 'Применить к теме';
+$lang['CDN_SITE_ROOT'] = 'Корень сайта';
+$lang['CDN_KEEP_HTTPS'] = 'Оставлять HTTPS в URL-адресах';
+$lang['CDN_FILE_TYPES_DESC'] = 'Выбрать типы файлов, которые должны быть обработаны с Вашего сервера CDN.';
+$lang['CDN_IGNORE_FILES'] = 'Игнорировать файлы';
+$lang['CDN_FILE_TYPES'] = 'Типы файлов';
+$lang['CDN_EXTRA_FILETYPES'] = 'Дополнительные типы файлов';
+$lang['CDN_ENABLED_DESC'] = 'Позволяет легко интегрироваться с Content Delivery Network (CDN).';
+$lang['CDN_ENABLED'] = 'Включить CDN';
+$lang['CDN_DOMAIN_DESC'] = 'Домен (хост) Вашего сервера CDN';
+$lang['CDN_DOMAIN'] = 'Домен CDN';
