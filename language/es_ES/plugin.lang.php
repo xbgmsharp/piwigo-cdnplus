0b83f4a55022dd01abdd1d21cd66ca8de2f112a1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-04 21:39:14 +0100

    Update es_ES, thanks to jpr928

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -1,21 +1,21 @@
 <?php
-$lang['PLG_CDNPLUS'] = "This plugin allows an easy integration with a Content Delivery Network (CDN) for your gallery.";
+$lang['PLG_CDNPLUS'] = 'Este plugin permite una fácil integración con una red de entrega de contenido (CDN) para su galería.';
 
-$lang['CDN_ENABLED'] = "Enabled CDN";
-$lang['CDN_ENABLED_DESC'] = "Allows for easy integration with a Content Delivery Network (CDN).";
+$lang['CDN_ENABLED'] = 'CDN activado';
+$lang['CDN_ENABLED_DESC'] = 'Permite una fácil integración con una red de devolución de contenido (CDN).';
 
-$lang['CDN_USE_EXTRA'] = "Multiple CDN roots";
+$lang['CDN_USE_EXTRA'] = 'Direcciones principales CDN';
 $lang['CDN_USE_EXTRA_DESC'] = "Seleccione para configurar otra Red de Entrega de Contenidos. De está manera usted puede configurar las diferentes redes de entrega de contenido para distribuir diferentes tipos de archivos, como por ejemplo uno para imágenes y otro para archivos js/css.";
 
-$lang['CDN_SITE_ROOT'] = "Directorio Raíz o Principal";
-$lang['CDN_SITE_ROOT_DESC'] = "Directorio principal de su sitio web que usted tiene conectado a su servidor CDN.<br />You probably don't have to change this, but if you need you can specify a sub folder like /images/.";
+$lang['CDN_SITE_ROOT'] = 'Sitio web Principal';
+$lang['CDN_SITE_ROOT_DESC'] = 'Directorio principal de su sitio web que usted tiene conectado a su servidor CDN.<br />Es probable que no tiene que cambiar esto, pero si usted lo necesita puede especificar una subcarpeta como / imagen /.';
 
 $lang['CDN_DOMAIN'] = "Dominio CDN";
-$lang['CDN_DOMAIN_DESC'] = "Dominio de su servidor CDN";
-$lang['CDN_DOMAINS_DESC'] = "The domain of your CDN Server.<br />You can also give a comma separated list of different CDN domains. One of these will be appointed to each file in the set at random.<br /><br />You can use the place holders {subdomain}, {domain} and {extension} to refer to the different parts of the domain name of your website.";
+$lang['CDN_DOMAIN_DESC'] = 'El dominio de su servidor CDN';
+$lang['CDN_DOMAINS_DESC'] = 'El dominio de su servidor CDN.<br />También se puede dar una lista separada por comas de los diferentes dominios de CDN. Uno de ellos será designado para cada archivo en el conjunto al azar.<br /><br />Puede utilizar los marcadores de posición {subdominio}, {dominio} y {extensión} para referirse a las diferentes partes del nombre de dominio de su sitio web.';
 
 $lang['CDN_EXTRA_FILETYPES'] = "Tipo de Archivos Adicionales";
-$lang['CDN_EXTRA_FILETYPES_DESC'] = "Tipos de Archivos separados por comas que se deben distribuir desde su servidor CDN (adicionales a los mencionados arriba), eg: js,css";
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'Tipos de Archivos separados por comas que se deben distribuir desde su servidor CDN (adicionales a los mencionados arriba), por ejemplo: js,css';
 
 $lang['CDN_FILE_TYPES'] = "Tipo de Archivos";
 $lang['CDN_FILE_TYPES_DESC'] = "Seleccione los tipos de archivos que deben ser distribuidos desde su servidor CDN.";
@@ -23,11 +23,13 @@ $lang['CDN_FILE_TYPES_DESC'] = "Seleccione los tipos de archivos que deben ser d
 $lang['CDN_IGNORE_FILES'] = "Ignorar Archivos";
 $lang['CDN_IGNORE_FILES_DESC'] = "Una lista de archivos/directorios a ignorar separados por comas (la dirección de estos archivos o directorios no serán cambiados a la de su servidor CDN).";
 
-$lang['CDN_KEEP_HTTPS'] = "Keep HTTPS in urls";
-$lang['CDN_KEEP_HTTPS_DESC'] = "Select to keep the https in the cdn urls. Otherwise the cdn urls will use http.";
+$lang['CDN_KEEP_HTTPS'] = 'Dejar HTTPS en las direcciones urls';
+$lang['CDN_KEEP_HTTPS_DESC'] = 'Seleccionar para mantener el https en el urls cdn. De lo contrario las urls cdn utilizarán http.';
 
-$lang['CDN_USE'] = "CDN Options";
+$lang['CDN_USE'] = 'Opciones CDN';
 $lang['CDN_USE_EXTRA_2'] = "Utilizar un 2do CDN";
 $lang['CDN_USE_EXTRA_3'] = "Utilizar un 3er CDN";
 $lang['CDN_USE_EXTRA_4'] = "Utilizar un 4to CDN";
 $lang['CDN_USE_EXTRA_5'] = "Utilizar un 5to CDN";
+$lang['CDN_THEME'] = 'Aplicar sobre el tema';
+$lang['CDN_THEME_DESC'] = 'Exportar los archivos de temas, por ejemplo: js, css.';
