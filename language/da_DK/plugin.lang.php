61d439b5b46b6f0d4287529979735bcfacf0703d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-04 18:00:04 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CDN_USE_EXTRA_3'] = 'Anvend tredje CDN-sæt';
+$lang['CDN_USE_EXTRA_4'] = 'Anvend fjerde CDN-sæt';
+$lang['CDN_USE_EXTRA_5'] = 'Anvend femte CDN-sæt';
+$lang['CDN_USE_EXTRA_DESC'] = 'Vælg for at aktivere ekstra Content Delivery Network-opsætning. På den måde kan du anvende forskellige filtyper, så som separate indstillinger vedrørende billed- og videofiler.';
+$lang['PLG_CDNPLUS'] = 'Plugin\'en gør det let at integrere et Content Delivery Network (CDN) med dit galleri.';
+$lang['CDN_DOMAIN'] = 'CDN-domæne';
+$lang['CDN_DOMAINS_DESC'] = 'Din CDN-servers domæne.<br />Du kan også lave en kommasepareret liste med forskellige CDN-domæner. Et af dem vil vil blive tilfældigt valgt i hver fil i sættet.<br /><br />Du kan anvende variablerne {subdomain}, {domain} og {extension} til at referere til de forskellige dele i dit websteds domæne.';
+$lang['CDN_DOMAIN_DESC'] = 'Din CDN-servers domæne (vært)';
+$lang['CDN_ENABLED'] = 'Aktive CDN\'er';
+$lang['CDN_ENABLED_DESC'] = 'Gør det let at integrere med et Content Delivery Network (CDN).';
+$lang['CDN_EXTRA_FILETYPES'] = 'Ekstra filtyper';
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'En kommasepareret liste over ekstra filtyper, der skal serveres fra din CDN-server, fx: png,jpg';
+$lang['CDN_FILE_TYPES'] = 'Filtyper';
+$lang['CDN_FILE_TYPES_DESC'] = 'Vælg filtyperne, der skal serveres fra din CDN-server.';
+$lang['CDN_IGNORE_FILES'] = 'Ignorer filer';
+$lang['CDN_IGNORE_FILES_DESC'] = 'En kommasperareret liste over (dele af) stinavne/filnavne, der skal ignoreres (URL\'er bliver ikke ændret til CDN-serveren).';
+$lang['CDN_KEEP_HTTPS'] = 'Bevar HTTPS i URL\'er';
+$lang['CDN_KEEP_HTTPS_DESC'] = 'Du kan vælge at bevare HTTPS i CDN-URL\'er.  Ellers vil CDN-URL\'erne indeholde HTTP.';
+$lang['CDN_SITE_ROOT'] = 'Webstedsrod';
+$lang['CDN_SITE_ROOT_DESC'] = 'Roden på dit websted, som er forbundet til din CDN-server.<br />Det skal formentlig ikke ændres, men om nødvendigt kan du angive en undermappe som eksempelvis /images/.';
+$lang['CDN_THEME'] = 'Benyt i tema';
+$lang['CDN_THEME_DESC'] = 'Eksporter temafiler, fx: js,css.';
+$lang['CDN_USE'] = 'CDN-valgmuligheder';
+$lang['CDN_USE_EXTRA'] = 'Flere CDN-webrødder';
+$lang['CDN_USE_EXTRA_2'] = 'Anvendt andet CDN-sæt';
