8d7a373fca772cd4146effc18e19d503372d6e6f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-05 18:06:37 +0100

    Update sk_SK, thanks to dodo

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CDN_DOMAINS_DESC'] = 'Doména vášho CDN servera.<br/>Môžete tiež zadať čiarkou oddelený zoznam rôznych domén CDN. Jeden z nich bude pridelený každému súboru v sade v náhodnom poradí.<br/><br/>Môžete použiť zástupcov {subdoména}, {doména} a {rozšírenie} na odkazovanie rôznych častí názvu domény na svojich webových stránkach.';
+$lang['CDN_SITE_ROOT_DESC'] = 'Root Vašich webových stránok, ktorými ste pripojený k serveru CDN.<br/> Pravdepodobne to nemôžete zmeniť, ale ak budete potrebovať, môžete zadať podadresár, ako je /images/.';
+$lang['CDN_USE_EXTRA_DESC'] = 'Označte na povolenie ďalšieho obsahu nastavenia Delivery Network. Týmto spôsobom môžete použiť rôzne nastavenia pre rôzne typy súborov, rovnako ako samostatné nastavenie pre obrázky a súbory videa.';
+$lang['CDN_FILE_TYPES_DESC'] = 'Vyberte typy súborov, ktoré by mali byť doručované zo servera CDN.';
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'Čiarkou oddelený zoznam nadštandardných typov súborov, ktoré by mali byť poskytované z CDN servera, napríklad: PNG, JPG';
+$lang['CDN_ENABLED_DESC'] = 'Umožňuje jednoduchú integráciu s Content Delivery Network (CDN).';
+$lang['CDN_IGNORE_FILES_DESC'] = 'Čiarkou oddelený zoznam (časť) cesty/názvov súborov budú ignorované (URL adries nebude zmenený na CDN serveri).';
+$lang['CDN_KEEP_HTTPS'] = 'Majte HTTPS v URL';
+$lang['CDN_KEEP_HTTPS_DESC'] = 'Vyberte udržanie https v CDN URL. Inak CDN URL adresy budú používať http.';
+$lang['CDN_USE_EXTRA'] = 'Viac CDN rootov';
+$lang['PLG_CDNPLUS'] = 'Tento doplnok umožňuje jednoduchú integráciu s Content Delivery Network (CDN) pre galériu.';
+$lang['CDN_USE_EXTRA_5'] = 'Použiť 5. nastavenie CDN';
+$lang['CDN_USE_EXTRA_4'] = 'Použiť 4. nastavenie CDN';
+$lang['CDN_USE_EXTRA_3'] = 'Použiť 3. nastavenie CDN';
+$lang['CDN_USE_EXTRA_2'] = 'Použiť 2. nastavenie CDN';
+$lang['CDN_USE'] = 'Voľby CDN';
+$lang['CDN_THEME_DESC'] = 'Exportovať súbory témy, ako: js, css.';
+$lang['CDN_THEME'] = 'Pridať do témy';
+$lang['CDN_SITE_ROOT'] = 'Root stránka';
+$lang['CDN_IGNORE_FILES'] = 'Ignorovať súbory';
+$lang['CDN_FILE_TYPES'] = 'Typy súborov';
+$lang['CDN_EXTRA_FILETYPES'] = 'Extra typy súborov';
+$lang['CDN_ENABLED'] = 'Povoliť CDN';
+$lang['CDN_DOMAIN_DESC'] = 'Doména Vášho CDN Servera';
+$lang['CDN_DOMAIN'] = 'CDN doména';
