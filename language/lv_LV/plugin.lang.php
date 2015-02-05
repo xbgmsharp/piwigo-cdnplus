0268de17628d9c195ccc19e346e6bd078a526a8c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-05 18:06:33 +0100

    Update lv_LV, thanks to agrisans

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CDN_THEME_DESC'] = 'Eksportēt tēmu failus, kā:js,css. ';
+$lang['CDN_USE_EXTRA_2'] = 'Pielietot otru CDN iestatījumu';
+$lang['CDN_USE_EXTRA'] = 'Vairākas CDN saknes (roots)';
+$lang['CDN_USE'] = 'CDN Opcijas';
+$lang['CDN_THEME'] = 'Pielietot tēmu';
+$lang['CDN_SITE_ROOT'] = 'Vietnes Sakne (Root)';
+$lang['CDN_IGNORE_FILES'] = 'Ignorēt Failus';
+$lang['CDN_FILE_TYPES'] = 'Failu Tipi';
+$lang['CDN_EXTRA_FILETYPES'] = 'Papildus Failu tipi';
+$lang['CDN_DOMAIN_DESC'] = 'Jūsu CDN Servera domēns (hosts)';
+$lang['CDN_ENABLED'] = 'Iespējot CDN';
+$lang['CDN_DOMAIN'] = 'CDN Domēns';
