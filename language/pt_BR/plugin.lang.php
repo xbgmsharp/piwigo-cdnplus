8d1f45452e46ce261a0542e0d9e963b75074f94f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-09 18:04:25 +0100

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PLG_CDNPLUS'] = 'Este plugin permite uma integração fácil com um Content Delivery Network(Rede de Distribuição de Conteúdos) (CDN) para a sua galeria.';
+$lang['CDN_USE_EXTRA_DESC'] = 'Selecione para permitir uma configuração de rede de entrega de conteúdo adicional. Desta forma, você pode usar configurações diferentes para diferentes tipos de arquivos, como ajustes separados para imagens e arquivos de vídeos.';
+$lang['CDN_USE_EXTRA_5'] = 'Use o quinto CDN configurado';
+$lang['CDN_USE_EXTRA_4'] = 'Use o quarto CDN configurado';
+$lang['CDN_USE_EXTRA_3'] = 'Use o terceiro CDN configurado';
+$lang['CDN_USE_EXTRA_2'] = 'Use o segundo CDN configurado';
+$lang['CDN_USE_EXTRA'] = 'Múltiplas raízes CDN';
+$lang['CDN_USE'] = 'Opções CDN';
+$lang['CDN_THEME_DESC'] = 'Exportar os arquivos do tema, por exemplo: js, css.';
+$lang['CDN_THEME'] = 'Aplicar para tema';
+$lang['CDN_SITE_ROOT_DESC'] = 'Raiz do seu site que você conectou ao seu servidor CDN. <br/> Você provavelmente não tem que mudar isso, mas se você precisar, você pode especificar uma pasta como sub/images/.';
+$lang['CDN_SITE_ROOT'] = 'Raiz do site';
+$lang['CDN_KEEP_HTTPS_DESC'] = 'Selecione para manter os https no urls CDN. Caso contrário, as urls CDN usarão http.';
+$lang['CDN_KEEP_HTTPS'] = 'Mantenha HTTPS em urls';
+$lang['CDN_IGNORE_FILES_DESC'] = 'A lista separada por vírgula de (parte de) nomes caminho/arquivos a ignorar (urls não serão alteradas para o servidor CDN).';
+$lang['CDN_IGNORE_FILES'] = 'Ignorar arquivos';
+$lang['CDN_FILE_TYPES_DESC'] = 'Selecione os tipos de arquivos que devem ser servidos a partir de seu servidor CDN.';
+$lang['CDN_FILE_TYPES'] = 'Tipos de Arquivos';
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'Uma lista separada por vírgula de tipos de arquivos extras que devem ser servidos a partir de seu servidor CDN, por exemplo: png, jpg';
+$lang['CDN_EXTRA_FILETYPES'] = 'Tipos de Arquivos extra';
+$lang['CDN_ENABLED_DESC'] = 'Permite uma fácil integração com uma Content Delivery Network(Rede de Distribuição de Conteúdos) (CDN).';
+$lang['CDN_ENABLED'] = 'CDN ativado';
+$lang['CDN_DOMAIN_DESC'] = 'O domínio (host) do servidor CDN';
+$lang['CDN_DOMAINS_DESC'] = 'Domínio do seu CDN Server. <br/>Você também pode dar uma lista separada por vírgula de diferentes domínios CDN. Um deles será designado para cada arquivo no conjunto de forma aleatória.<br/><br/>Você pode utilizar o lugar titulares {subdomínio}, {domínio} e {extensão} para se referir às diferentes partes do nome de domínio do seu site.';
+$lang['CDN_DOMAIN'] = 'Domínnio CDN';
