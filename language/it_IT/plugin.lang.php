a0ff5d8ae2e3e6e19feb78abf004c5f856b0ac7f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-10 21:10:01 +0100

    Update it_IT, thanks to Ericnet

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CDN_DOMAINS_DESC'] = 'Il dominio del Server CDN.<br/>Puoi anche dare un elenco separato da virgole dei diversi domini CDN. Uno di questi sarà nominato per ogni file nel set a caso.<br/><br/>Puoi utilizzare i segnaposto {sottodominio}, {dominio} eed {estensione} per indicare le diverse parti del nome di dominio del tuo sito web.';
+$lang['CDN_SITE_ROOT_DESC'] = 'La root del tuo sito web che è stato collegato al server CDN.<br/>Probabilmente non c\'è bisogno di cambiare questo, ma se hai bisogno puoi specificare una cartella secondaria come /images/.';
+$lang['CDN_USE_EXTRA_DESC'] = 'Seleziona per abilitare una configurazione supplementare per Content Delivery Network. In questo modo è possibile utilizzare impostazioni diverse per i diversi tipi di file, come le impostazioni separate per le immagini e file video.';
+$lang['CDN_ENABLED_DESC'] = 'Consente una facile integrazione con una Content Delivery Network (CDN).';
+$lang['PLG_CDNPLUS'] = 'Questo plugin permette una facile integrazione con una Content Delivery Network (CDN) per la tua galleria.';
+$lang['CDN_THEME_DESC'] = 'Esporta i file del tema, esempio: js,css.';
+$lang['CDN_IGNORE_FILES_DESC'] = 'Un elenco separato di (parte di) nomi di percorso/file da ignorare (url non verranno modificati dal server CDN).';
+$lang['CDN_THEME'] = 'Applica a tema';
+$lang['CDN_SITE_ROOT'] = 'Root Sito';
+$lang['CDN_KEEP_HTTPS_DESC'] = 'Seleziona per mantenere https negli url cdn. In caso contrario, gli url cdn utilizzeranno http.';
+$lang['CDN_KEEP_HTTPS'] = 'Mantieni HTTPS negli URL';
+$lang['CDN_IGNORE_FILES'] = 'Ignora file';
+$lang['CDN_FILE_TYPES_DESC'] = 'Seleziona i tipi di file che devono essere serviti dal server CDN.';
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'Un extra elenco dei tipi di file separato da virgola che dovrebbe essere servita dal server CDN, esempio: png,jpg';
+$lang['CDN_FILE_TYPES'] = 'Tipi di file';
+$lang['CDN_USE_EXTRA_2'] = 'Utilizzare 2^ set CDN';
+$lang['CDN_USE_EXTRA_3'] = 'Utilizzare 3^ set CDN';
+$lang['CDN_USE_EXTRA_4'] = 'Utilizzare 4^ set CDN';
+$lang['CDN_USE_EXTRA_5'] = 'Utilizzare 5^ set CDN';
+$lang['CDN_USE_EXTRA'] = 'Multiple CDN roots';
+$lang['CDN_USE'] = 'Opzioni CDN';
+$lang['CDN_EXTRA_FILETYPES'] = 'Tipi di file Extra';
+$lang['CDN_DOMAIN_DESC'] = 'Il dominio (host) del server CDN';
+$lang['CDN_ENABLED'] = 'CDN attivato';
+$lang['CDN_DOMAIN'] = 'Dominio CDN';
