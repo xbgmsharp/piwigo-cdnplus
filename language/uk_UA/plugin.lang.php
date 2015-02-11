071f4d238e7e6af80861b6252ee6e455d27d5359
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-11 17:13:22 +0100

    Update uk_UA, thanks to m_alien

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CDN_DOMAINS_DESC'] = 'Домен вашого CDN серверу.<br />Ви також можете дати розділений комами список різних доменів CDN. Один з них випадковим чином буде призначений до кожного файлу з набору.<br /><br />Ви можете використовувати плейсхолдери (заглушки) {subdomain}, {domain} та {extension} щоб звернутися до різних частин доменого імені вашого сайту.';
+$lang['CDN_USE_EXTRA_5'] = 'Використовувати 5-ий набір CDN';
+$lang['CDN_USE_EXTRA_4'] = 'Використовувати 4-ий набір CDN';
+$lang['CDN_USE_EXTRA_3'] = 'Використовувати 3-ій набір CDN';
+$lang['CDN_USE_EXTRA_2'] = 'Використовувати 2-ий набір CDN';
+$lang['CDN_USE_EXTRA'] = 'Кілька коренів CDN';
+$lang['CDN_USE'] = 'Налаштування CDN';
+$lang['CDN_THEME_DESC'] = 'Експортувати файли теми, напр.: js,css.';
+$lang['CDN_THEME'] = 'Застосувати до теми';
+$lang['CDN_SITE_ROOT_DESC'] = 'Корінь вашого вебсайту, який ви підключили до вашого CDN серверу.<br />Ймовірно вам не потрібно це змінювати, але якщо необхідно, ви можете вказати підпапку як /images/.';
+$lang['CDN_SITE_ROOT'] = 'Корінь сайту';
+$lang['CDN_KEEP_HTTPS_DESC'] = 'Виберіть, щоб тримати https у cdn url-ах. Інакше cdn url-и будуть використовувати http.';
+$lang['CDN_KEEP_HTTPS'] = 'Тримати HTTPS у url-ах';
+$lang['CDN_IGNORE_FILES_DESC'] = 'Розділений комами список (частина) каталог/ім\'я файлу для ігнорування (url-и не будуть змінюватись на сервері CDN).';
+$lang['CDN_ENABLED'] = 'Активований CDN';
+$lang['CDN_IGNORE_FILES'] = 'Ігнорувати файли';
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'Розділений комами список додаткових типів файлів, які повинні оброблятися вашим CDN сервером, напр.: png,jpg';
+$lang['CDN_FILE_TYPES_DESC'] = 'Оберіть типи файлів, які повинні бути подані з вашого CDN серверу.';
+$lang['CDN_FILE_TYPES'] = 'Типи файлів';
+$lang['CDN_EXTRA_FILETYPES'] = 'Додаткові типи файлів';
+$lang['CDN_ENABLED_DESC'] = 'Дозволяє легку інтеграцію з мережею доставки контенту (CDN).';
+$lang['CDN_DOMAIN_DESC'] = 'Домен (хост) вашого CDN серверу';
+$lang['CDN_DOMAIN'] = 'CDN домен';
+$lang['CDN_USE_EXTRA_DESC'] = 'Виберіть, щоб включити додаткове налаштування Мережі Доставки Контенту (CDN). Таким чином, ви можете використовувати різні налаштування для різних типів файлів, наприклад окремі параметри для зображень і відео файлів.';
+$lang['PLG_CDNPLUS'] = 'Цей плагін дозволяє легко інтегрувати вашу галерею з Мережею Доставки Контенту (CDN).';
