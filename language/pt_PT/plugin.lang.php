a9e7d8cf303d21c3e580e9f259f6c26011091923
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-19 06:53:36 +0100

    Update pt_PT, thanks to vn

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CDN_SITE_ROOT_DESC'] = 'A raiz do seu sítio, que ligou ao seu servidor CDN.<br />Provavelmtente não terá de alterar isto, mas se necessitar pode especificar uma sub pasta como /imagens/.';
+$lang['CDN_SITE_ROOT'] = 'Raíz do sítio';
+$lang['CDN_KEEP_HTTPS_DESC'] = 'Seleccione para manter https nos endereços do CDN. Caso contrário os endereços do CDN usarão http';
+$lang['CDN_KEEP_HTTPS'] = 'Mantenha HTTPS nos endereços';
+$lang['CDN_IGNORE_FILES_DESC'] = 'Lista separada por vírgulas de (ou parte de) nomes path/files a ignorar (endereços não serão alterados para o servidor CDN)';
+$lang['CDN_IGNORE_FILES'] = 'Ignore os ficheiros';
+$lang['CDN_FILE_TYPES_DESC'] = 'Seleccione os tipos de ficheiros que serão servidos pelo seu servidor CDN';
+$lang['CDN_EXTRA_FILETYPES'] = 'Tipos de ficheiro extra';
+$lang['CDN_FILE_TYPES'] = 'Tipos de ficheiro';
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'A separação por vírgulas dos tipos de ficheiros extra deverão ser servidos pelo seu servidor CDN, p.e.: png, jpg';
+$lang['CDN_ENABLED_DESC'] = 'Permite uma integração fácil no Conteúdo da Rede de Distribuição (CDN)';
+$lang['CDN_ENABLED'] = 'activa o CDN';
+$lang['CDN_DOMAIN_DESC'] = 'O domínio (alojador) do seu servidor CDN';
+$lang['CDN_DOMAINS_DESC'] = 'O domínio do seu servidor CDN.<br />Pode ainda integrar uma lista de difrentes domínios CDN separados por vírgulas. Cada um deles será apontado para o ficheiro respectivo na definição de aleatório.<br /><br />Pode recorrer aos suportes de local (place holders) {subdomínio}, {domínio} and {extensão} para apontar para as diferentes zonas do domínio do seu sítio (website).';
+$lang['CDN_DOMAIN'] = 'Domínio CDN';
