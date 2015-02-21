2f3d3e55ed1e92031287148c5f721eb0e096a335
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-21 07:25:50 +0100

    Update el_GR, thanks to bas_alba

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CDN_FILE_TYPES'] = 'Τύποι αρχείων';
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'Μια λίστα με διαχωρισμένες με κόμμα των επιπλέον τύπων αρχείων που πρέπει να εξυπηρετούνται από τον CDN server σας, π.χ.: png, jpg';
+$lang['CDN_EXTRA_FILETYPES'] = 'Επιμ πλέον Τύποι αρχείων';
+$lang['CDN_ENABLED_DESC'] = 'Επιτρέπει την εύκολη ενσωμάτωση με ένα Δίκτυο Παροχής Περιεχομένου (CDN).';
+$lang['CDN_ENABLED'] = 'Ενεργοποιημένο CDN';
+$lang['CDN_DOMAIN_DESC'] = 'Ο τομέας (φιλοξενίας) του CDN διακομιστή σας';
+$lang['CDN_DOMAINS_DESC'] = 'Ο τομέας του CDN διακομιστή σας.<br/>  Μπορείτε επίσης να δώσετε μια λίστα με διαχωρισμένους με κόμμα των διαφορετικούς τομείς CDN. Ένας από αυτούς θα ορίζετε από κάθε αρχείο στο σετ τυχαία.<br /><br />Μπορείτε να χρησιμοποιήσετε τους place holders {subdomain}, {domain} και {extension} να αναφέρονται στα διάφορα μέρη του domain name της ιστοσελίδας σας.';
+$lang['CDN_DOMAIN'] = 'CDN Domain';
