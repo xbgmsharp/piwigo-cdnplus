47e2dea65f5e4363f9e07ac66614f8d23fe7fb15
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-03-15 21:41:18 +0100

    Add sv_SE, thanks to adrian_vg

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CDN_DOMAINS_DESC'] = 'Din CDN-servers domän.<br />Du kan också ange en kommaseparerad list med olika CDN-domäner. En av dessa kommer att slumpmässigt appliceras till varje fil i setet.<br /><br />Du kan använda platshållarna {subdomain}, {domain} och {extension} för att referera till olika delar av webbsajts domännamn.';
+$lang['CDN_SITE_ROOT_DESC'] = 'Din webbsajts rot som du anslutit till din CDN-server.<br/> Du behöver antagligen inte ändra detta, men om det behövs går det att ange en underfolder som t.ex. /bilder/.';
+$lang['CDN_USE_EXTRA_DESC'] = 'Välj att aktivera en konfigurering för ett extra nätverk för leverans av innehåll. På detta sätt kan du välja att använda olika inställningar för olika filtyper, som t.ex. separata inställningar för bilder och video-filer.';
+$lang['PLG_CDNPLUS'] = 'Denna plugin tillåter en enkel integration med ett nätverk för leverans av innehåll (CDN) för ditt galleri.';
+$lang['CDN_USE_EXTRA_3'] = 'Använd tredje CDN-setet.';
+$lang['CDN_USE_EXTRA_4'] = 'Använd fjärde CDN-setet.';
+$lang['CDN_USE_EXTRA_5'] = 'Använd femte CDN-setet.';
+$lang['CDN_THEME_DESC'] = 'Exportera tema-filerna, t.ex. js, css.';
+$lang['CDN_KEEP_HTTPS_DESC'] = 'Välj att behålla https i CDN-URL:en. Om inte, kommer CDN-URL:arna att använda http.';
+$lang['CDN_ENABLED_DESC'] = 'Tillåter enkel integration med ett nätverk för leverans av innehåll (CDN).';
+$lang['CDN_DOMAIN_DESC'] = 'Din CDN-servers domän (host/värd)';
+$lang['CDN_DOMAIN'] = 'CDN-domän';
+$lang['CDN_THEME'] = 'Applicera till temat';
+$lang['CDN_USE_EXTRA_2'] = 'Använd det andra CDN-setet';
+$lang['CDN_USE_EXTRA'] = 'Multipla CDN-rötter';
+$lang['CDN_USE'] = 'CDN-alternativ';
+$lang['CDN_SITE_ROOT'] = 'Sajtrot';
+$lang['CDN_KEEP_HTTPS'] = 'Behåll HTTPS i URL:en';
+$lang['CDN_IGNORE_FILES_DESC'] = 'En kommaseparerad lista av (alt. delar av) sökvägar/filnamn som ska ignoreras (URL:ar kommer inte att ändras till CDN-server).';
+$lang['CDN_IGNORE_FILES'] = 'Ignorera filer';
+$lang['CDN_FILE_TYPES_DESC'] = 'Välj filtyperna som ska erbjudas från din CDN-server.';
+$lang['CDN_ENABLED'] = 'Aktiverad CDN';
+$lang['CDN_FILE_TYPES'] = 'Filtyper';
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'En kommaseparerad lista med extra filtyper som ska erbjudas från din CDN-server, t.ex. png, jpg';
+$lang['CDN_EXTRA_FILETYPES'] = 'Extra filtyper';
