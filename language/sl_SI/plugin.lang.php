0dc2bc9131b0181663653b85bcad8f58ab9ba531
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-15 21:26:42 +0100

    Add sl_SI, thanks to MarkoAm

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['CDN_USE_EXTRA_3'] = 'Uporabi tretji CDN nabor';
+$lang['CDN_USE_EXTRA_4'] = 'Uporabi četrti CDN nabor';
+$lang['CDN_USE_EXTRA_5'] = 'Uporabi peti CDN nabor';
+$lang['CDN_DOMAIN'] = 'CDN Domena';
+$lang['CDN_DOMAIN_DESC'] = 'Domenski naslov (host) vašega CDN Strežnika';
+$lang['CDN_ENABLED'] = 'Omogoči CDN';
+$lang['CDN_ENABLED_DESC'] = 'Omogoča preprosto integracijo z Content Delivery Network (CDN).';
+$lang['CDN_EXTRA_FILETYPES'] = 'Dodatne vrste datotek';
+$lang['CDN_EXTRA_FILETYPES_DESC'] = 'Z vejico ločen spisek dodatnih vrst datotek ki naj bodo strežene preko CDN strežnika, npr.: png, jpg';
+$lang['CDN_FILE_TYPES'] = 'Vrste datotek';
+$lang['CDN_FILE_TYPES_DESC'] = 'Izberite vrsto datotek ki naj bodo strežene preko vašega CDN strežnika';
+$lang['CDN_IGNORE_FILES'] = 'Prezri datoteke';
+$lang['CDN_IGNORE_FILES_DESC'] = 'Z vejico ločen spisek imen (delnih imen) pot/ime datoteke ki naj bo spregledan (URL-ji ne bodo spremenjeni na CDN stržniku).';
+$lang['CDN_KEEP_HTTPS'] = 'Ohrani HTTPS v URL-jih';
+$lang['CDN_KEEP_HTTPS_DESC'] = 'Izberite ohranjanje https v CDN URL-jih. V nasprotnem bodo CDN URL-ji uporabili http';
+$lang['CDN_SITE_ROOT'] = 'Korenski imenik strani';
+$lang['CDN_SITE_ROOT_DESC'] = 'Korenski imenik vaše spletne strani ki ste jo povezali z CDN strežnikom.<br />Vrjetno vam tega ni potrebno spreminjati, če pa morate pa lahko določite poddirektorij kot naprimer /fotografije/';
+$lang['CDN_THEME'] = 'Uveljavi na preobleki';
+$lang['CDN_THEME_DESC'] = 'Izvozi datoteke preobleke, kot naprimer: js, css.';
+$lang['CDN_USE'] = 'CDN možnosti';
+$lang['CDN_USE_EXTRA'] = 'Več korenski CDN';
+$lang['CDN_USE_EXTRA_2'] = 'Uporabi drugi CDN nabor';
