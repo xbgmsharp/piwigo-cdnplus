c3a12c37ad1dc081a7df1d0754c7efdeec2e57d6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-08-20 07:56:59 +0200

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -20,17 +20,17 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['CDN_USE_EXTRA_DESC'] = 'Selecteer om een extra Content Delivery Network setup mogelijk te maken. Op deze manier kunt u verschillende instellingen gebruiken voor verschillende bestandstypen, zoals aparte instellingen voor afbeeldingen en video-bestanden.';
-$lang['CDN_DOMAINS_DESC'] = 'Het domein van uw CDN-server.<br/>U kunt ook een door komma\'s gescheiden lijst van verschillende CDN domeinen aangeven. Een van deze zal worden toegewezen aan elk willekeurig bestand in de set.<br/><br/>U kunt de plaatshouders {subdomein}, {domain} en {extensie} gebruiken om te verwijzen naar de verschillende delen van de domeinnaam van uw website.';
+$lang['CDN_USE_EXTRA_DESC'] = 'Selecteer om een extra Content Delivery Network setup mogelijk te maken. Op deze manier kun je verschillende instellingen gebruiken voor verschillende bestandstypen, zoals aparte instellingen voor afbeeldingen en video-bestanden.';
+$lang['CDN_DOMAINS_DESC'] = 'Het domein van uw CDN-server.<br/>Je kunt ook een door komma\'s gescheiden lijst van verschillende CDN domeinen aangeven. Een van deze zal worden toegewezen aan elk willekeurig bestand in de set.<br/><br/>Je kunt de plaatshouders {subdomein}, {domain} en {extensie} gebruiken om te verwijzen naar de verschillende delen van de domeinnaam van jouw website.';
 $lang['CDN_DOMAIN'] = 'CDN Domein';
-$lang['CDN_DOMAIN_DESC'] = 'Het domein (host) van uw CDN Server';
+$lang['CDN_DOMAIN_DESC'] = 'Het domein (host) van jouw CDN Server';
 $lang['CDN_ENABLED'] = 'Ingeschakeld CDN';
 $lang['CDN_ENABLED_DESC'] = 'Zorgt voor eenvoudige integratie met een Content Delivery Network (CDN).';
 $lang['CDN_EXTRA_FILETYPES'] = 'Extra bestandstypen';
 $lang['CDN_EXTRA_FILETYPES_DESC'] = 'Een door komma\'s gescheiden lijst met extra bestandstypen die moeten worden bediend vanaf uw CDN server, bv: png, jpg';
 $lang['CDN_KEEP_HTTPS_DESC'] = 'Selecteer om https in de cdn url\'s te behouden. Anders zullen de cdn-url\'s http gebruiken.';
 $lang['CDN_SITE_ROOT'] = 'Website Root';
-$lang['CDN_SITE_ROOT_DESC'] = 'De hoofdmap (root) van uw website die u verbonden hebt met uw CDN server. <br/> U hoeft dit waarschijnlijk niet te veranderen, maar als het nodig mocht zijn, kunt u een submap aangeven, zoals /afbeeldingen/.';
+$lang['CDN_SITE_ROOT_DESC'] = 'De hoofdmap (root) van jouw website die je verbonden hebt met jouw CDN server. <br/> Je hoeft dit waarschijnlijk niet te veranderen, maar als het nodig mocht zijn, kun je een submap aangeven, zoals /afbeeldingen/.';
 $lang['CDN_THEME'] = 'Toepassen op thema';
 $lang['CDN_THEME_DESC'] = 'Exporteer de thema-bestanden, bv: js, css.';
 $lang['CDN_USE'] = 'CDN Opties';
@@ -41,7 +41,7 @@ $lang['CDN_USE_EXTRA_4'] = 'Gebruik 4e CDN set';
 $lang['CDN_USE_EXTRA_5'] = 'Gebruik 5e CDN set';
 $lang['PLG_CDNPLUS'] = 'Deze plugin maakt een eenvoudige integratie met een Content Delivery Network (CDN) voor uw galerij mogelijk.';
 $lang['CDN_FILE_TYPES'] = 'Bestandstypen';
-$lang['CDN_FILE_TYPES_DESC'] = 'Selecteer de bestandstypen die moeten worden bediend vanaf uw CDN server.';
+$lang['CDN_FILE_TYPES_DESC'] = 'Selecteer de bestandstypen die moeten worden bediend vanaf jouw CDN server.';
 $lang['CDN_IGNORE_FILES'] = 'Negeer bestanden';
 $lang['CDN_IGNORE_FILES_DESC'] = 'Een door komma\'s gescheiden lijst van te negeren pad/bestandsnamen (of een deel van pad/bestandsnamen)(url\'s zullen niet worden gewijzigd in CDN-server).';
 $lang['CDN_KEEP_HTTPS'] = 'Houd HTTPS in url\'s';
